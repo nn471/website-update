e3614e7b4dc482d3b15090404b1e3d768ee07bb4
nnpxxl <nnpxxl@nnpxxls-MacBook-Air.local>
2025-08-29 13:08:38 +0100

    made functional button that direct to different blocks

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,18 @@
-const returnButton = document.querySelector(".return-button");
-const returnButton = document.querySelector(".return-button");
+const blocks = document.querySelectorAll(".block");
+
+blocks[0].style.display = "block";
+
+let currentBlock = 0;
+let lastBlock = blocks.length - 1;
+
+function switchBlock(step) {
+  let newIndex = currentBlock + step;
+  if (newIndex < 0 || newIndex > lastBlock) return;
+
+  for (let i = 0; i < blocks.length; i++) {
+    document.getElementById(`block-${i + 1}`).style.display = "none";
+  }
+  blocks[newIndex].style.display = "block";
+  currentBlock = newIndex;
+  console.log(currentBlock);
+}
